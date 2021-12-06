acf3d82a8be9cb67afc16106c1d9ee649ec0a8c6
Sneha <sneha.a@netobjex.com>
2021-12-06 19:12:33 +0530

    added pwa configs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+// const withPWA = require('next-pwa')
+// const runtimeCaching = require('next-pwa/cache')
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: 'public',
+//     runtimeCaching,
+//   },
+// })
+
+
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
-    dest: 'public',
+    dest: "public",
+    register: true,
+    skipWaiting: true,
     runtimeCaching,
-  },
-})
+    buildExcludes: [/middleware-manifest.json$/]
+  }
+});
